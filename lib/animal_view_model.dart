6c90ab44a683a84b19cdf4a5952d79be7b7d955f
Fatdev <akhlaghi.fatemeh@gmail.com>
2022-10-15 17:42:36 +0330

    Add Base view model for cat & dog

---
+++ lib/animal_view_model.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:pet_service/home_view_model.dart';
+import 'package:provider/provider.dart';
+
+class AnimalViewModel extends ChangeNotifier {
+  bool _isGrooming = false;
+  bool _isReservingHotel = false;
+  int _nightsNumber = 0;
+
+  bool get isGrooming => _isGrooming;
+
+  bool get isReservingHotel => _isReservingHotel;
+
+  int get nightsNumber => _nightsNumber;
+
+  set isGrooming(bool value) {
+    if (value != _isGrooming) {
+      _isGrooming = value;
+      notifyListeners();
+    }
+  }
+
+  set isReservingHotel(bool value) {
+    if (value != _isReservingHotel) {
+      _isReservingHotel = value;
+      if(!_isReservingHotel) {
+        _nightsNumber = 0;
+      }
+      notifyListeners();
+    }
+  }
+
+  set nightsNumber(int value) {
+    if (value != _nightsNumber) {
+      _nightsNumber = value;
+      notifyListeners();
+    }
+  }
+
+  void changeGrooming() {
+    isGrooming = !isGrooming;
+  }
+
+  void changeReservingHotel() {
+    isReservingHotel = !isReservingHotel;
+  }
+
+  void increaseNights() {
+    nightsNumber++;
+  }
+
+  void decreaseNights() {
+    if (nightsNumber > 0) {
+      nightsNumber--;
+    }
+  }
+}
--- lib/dog_view_model.dart
+++ lib/dog_view_model.dart
@@ -1,70 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pet_service/animal_view_model.dart';
 import 'package:provider/provider.dart';
 
-class DogViewModel extends ChangeNotifier {
-  bool _isGrooming = false;
-  bool _isReservingHotel = false;
-  int _nightsNumber = 0;
-
-  bool get isGrooming => _isGrooming;
-
-  bool get isReservingHotel => _isReservingHotel;
-
-  int get nightsNumber => _nightsNumber;
-
-  set isGrooming(bool value) {
-    print("I am Dog");
-
-    print(_isGrooming);
-    if (value != _isGrooming) {
-      _isGrooming = value;
-      notifyListeners();
-    }
-    print(_isGrooming);
-  }
-
-  set isReservingHotel(bool value) {
-    print(_isReservingHotel);
-
-    if (value != _isReservingHotel) {
-      _isReservingHotel = value;
-      if(!_isReservingHotel) {
-        _nightsNumber = 0;
-      }
-      notifyListeners();
-    }
-    print(_isReservingHotel);
-  }
-
-  set nightsNumber(int value) {
-    print(_nightsNumber);
-
-    if (value != _nightsNumber) {
-      _nightsNumber = value;
-      notifyListeners();
-    }
-    print(_nightsNumber);
-  }
-
+class DogViewModel extends AnimalViewModel {
   static DogViewModel of(BuildContext context) =>
       Provider.of<DogViewModel>(context, listen: false);
-
-
-  void changeGrooming() {
-    isGrooming = !isGrooming;
-  }
-
-  void changeReservingHotel() {
-    isReservingHotel = !isReservingHotel;
-  }
-
-  void increaseNights() {
-    nightsNumber++;
-  }
-
-  void decreaseNights() {
-    if (nightsNumber > 0) {
-      nightsNumber--;
-    }
-  }
 }
