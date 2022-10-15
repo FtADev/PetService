18aa81bd649f8f121d1ba0dc274e25bc054bd8f9
Fatdev <akhlaghi.fatemeh@gmail.com>
2022-10-15 20:04:38 +0330

    Add ServiceRepository

---
+++ lib/data/repository/service_repository.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:pet_service/data/model/calculate_request_model.dart';
+import 'package:pet_service/data/model/calculate_result_model.dart';
+import 'package:pet_service/data/network/api/user/services_api.dart';
+import 'package:pet_service/data/network/dio_exception.dart';
+
+class ServiceRepository {
+  final ServicesApi serviceApi;
+
+  ServiceRepository(this.serviceApi);
+
+  Future<CalculateResultModel> calculateCostRequest(CalculateRequestModel req) async {
+    try {
+      final response = await serviceApi.calculateCost(req);
+      return CalculateResultModel.fromJson(response.data);
+    } on DioError catch (e) {
+      final errorMessage = DioExceptions.fromDioError(e).toString();
+      throw errorMessage;
+    }
+  }
+}
