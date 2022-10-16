19c91e7b523d898b16123d2190fb9fa4445d79c8
Fatdev <akhlaghi.fatemeh@gmail.com>
2022-10-16 18:15:20 +0330

    Change di

---
+++ lib/di/injection_container.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:pet_service/clean_arc/features/service_cost/data/datasources/cost_remote_data_source.dart';
+import 'package:pet_service/clean_arc/features/service_cost/data/repositories/service_repository_impl.dart';
+import 'package:pet_service/clean_arc/features/service_cost/domain/repositories/service_repository.dart';
+import 'package:pet_service/clean_arc/features/service_cost/domain/usecases/get_calculated_cost.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../clean_arc/core/dio_client.dart';
+import '../clean_arc/core/network/network_info.dart';
+
+
+final getIt = GetIt.instance;
+
+Future<void> init() async {
+  //! Features - Number Trivia
+  // Bloc
+  // getIt.registerFactory(
+  //   () => NumberTriviaBloc(
+  //     concrete: getIt(),
+  //     inputConverter: getIt(),
+  //     random: getIt(),
+  //   ),
+  // );
+
+  // Use cases
+  getIt.registerLazySingleton(() => GetCalculatedCost(getIt()));
+
+  // Repository
+  getIt.registerLazySingleton<ServiceRepository>(
+    () => ServiceRepositoryImpl(
+      // localDataSource: getIt(),
+      networkInfo: getIt(),
+      remoteDataSource: getIt(),
+    ),
+  );
+
+  // Data sources
+  getIt.registerLazySingleton<CostRemoteDataSource>(
+    () => CostRemoteDataSourceImpl(dioClient: getIt()),
+  );
+
+  // getIt.registerLazySingleton<NumberTriviaLocalDataSource>(
+  //   () => NumberTriviaLocalDataSourceImpl(sharedPreferences: getIt()),
+  // );
+
+  //! Core
+  // getIt.registerLazySingleton(() => InputConverter());
+  getIt.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(getIt()));
+
+  //! External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton(() => sharedPreferences);
+  getIt.registerLazySingleton(() => Dio(getIt()));
+  getIt.registerLazySingleton(() => DioClient(getIt()));
+  getIt.registerLazySingleton(() => InternetConnectionChecker());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:pet_service/data/repository/service_repository.dart';
-import 'package:pet_service/di/service_locator.dart';
+import 'package:pet_service/di/injection_container.dart';
 import 'package:pet_service/ui/view_models/cat_view_model.dart';
 import 'package:pet_service/ui/view_models/dog_view_model.dart';
 import 'package:pet_service/ui/home_screen.dart';
 import 'package:pet_service/ui/view_models/home_view_model.dart';
 import 'package:provider/provider.dart';
+import 'clean_arc/features/service_cost/domain/repositories/service_repository.dart';
+import 'di/injection_container.dart' as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await setup();
+  await di.init();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
-  final serviceRepository = getIt.get<ServiceRepository>();
+  final serviceRepository = getIt.get<ServiceRepository>(); //TODO(REMOVE)
 
   @override
   Widget build(BuildContext context) {
