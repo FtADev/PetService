f7b60f3c4fb9464498a239f709c33ea1cb04ddc1
Fatdev <akhlaghi.fatemeh@gmail.com>
2022-10-17 13:24:03 +0330

    Change Param in usecase

--- lib/clean_arc/features/service_cost/domain/usecases/get_calculated_cost.dart
+++ lib/clean_arc/features/service_cost/domain/usecases/get_calculated_cost.dart
@@ -1,23 +1,35 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:pet_service/clean_arc/features/service_cost/domain/entities/cost.dart';
-import 'package:pet_service/clean_arc/features/service_cost/domain/entities/request.dart';
 import 'package:pet_service/clean_arc/features/service_cost/domain/repositories/service_repository.dart';
 
 import '../../../../core/error/failures.dart';
 import '../../../../core/usecases/usecase.dart';
 
-class GetCalculatedCost implements UseCase<Cost, Request> {
+class GetCalculatedCost implements UseCase<Cost, Params> {
   final ServiceRepository repository;
 
   GetCalculatedCost(this.repository);
 
   @override
-  Future<Either<Failure, Cost>> call(Request request) async {
+  Future<Either<Failure, Cost>> call(Params params) async {
     return await repository.getCalculatedConst(
-      isCatGrooming: request.cat!.services!.grooming!,
-      catNights: request.cat!.services!.hotel!.nights!,
-      isDogGrooming: request.dog!.services!.grooming!,
-      dogNights: request.dog!.services!.hotel!.nights!,
+      isCatGrooming: params.isCatGrooming,
+      catNights: params.catNights,
+      isDogGrooming: params.isDogGrooming,
+      dogNights: params.dogNights,
     );
   }
+}
+
+class Params extends Equatable {
+  final bool isCatGrooming;
+  final int catNights;
+  final bool isDogGrooming;
+  final int dogNights;
+
+  const Params({required this.isCatGrooming, required this.catNights, required this.isDogGrooming, required this.dogNights,});
+
+  @override
+  List<Object> get props => [isCatGrooming, catNights, isDogGrooming, dogNights];
 }
