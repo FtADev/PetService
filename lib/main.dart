4c01613d764247c91e79b7110ca47cb0253d89ae
Fatdev <akhlaghi.fatemeh@gmail.com>
2022-10-17 13:25:54 +0330

    Change Home UI from provider to bloc

--- lib/main.dart
+++ lib/main.dart
@@ -16,29 +16,16 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
-  final serviceRepository = getIt.get<ServiceRepository>(); //TODO(REMOVE)
+  // final serviceRepository = getIt.get<ServiceRepository>(); //TODO(REMOVE)
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<HomeViewModel>(
-          create: (_) => HomeViewModel(serviceRepository),
-        ),
-        ChangeNotifierProvider<CatViewModel>(
-          create: (_) => CatViewModel(),
-        ),
-        ChangeNotifierProvider<DogViewModel>(
-          create: (_) => DogViewModel(),
-        ),
-      ],
-      child: MaterialApp(
+    return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: const HomeScreen(),
-      ),
-    );
+      );
   }
 }
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,66 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pet_service/clean_arc/features/service_cost/presentation/bloc/home/home_bloc.dart';
+import 'package:pet_service/clean_arc/features/service_cost/presentation/bloc/home/home_event.dart';
+import 'package:pet_service/clean_arc/features/service_cost/presentation/bloc/home/home_state.dart';
 import 'package:pet_service/ui/choose_pet_widget.dart';
-import 'package:pet_service/ui/view_models/cat_view_model.dart';
-import 'package:pet_service/ui/view_models/dog_view_model.dart';
-import 'package:pet_service/ui/view_models/home_view_model.dart';
-import 'package:provider/provider.dart';
+
+import '../di/injection_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    CatViewModel catModel = CatViewModel.of(context);
-    DogViewModel dogModel = DogViewModel.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pet Service"),
+      ),
+      body: buildBody(context),
+    );
+  }
 
-    return Consumer<HomeViewModel>(builder: (context, viewModel, child) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text("Pet Service"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const ChoosePetWidget(),
-              const SizedBox(
-                height: 50,
-              ),
-              viewModel.cost == 0
-                  ? Container()
-                  : Text(
-                      "\$ ${viewModel.cost}",
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
+  BlocProvider<HomeBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (_) => getIt<HomeBloc>(),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const ChoosePetWidget(),
+            const SizedBox(
+              height: 50,
+            ),
+            BlocBuilder<HomeBloc, HomeState>(
+              builder: (context, state) {
+                if (state is Loaded) {
+                  return Text(
+                    "\$ ${state.cost}",
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
                     ),
-              const SizedBox(
-                height: 8,
-              ),
-              ElevatedButton(
-                child: viewModel.isLoading
-                    ? Container(
-                        margin: const EdgeInsets.all(5),
-                        child: const CircularProgressIndicator(
-                          color: Colors.white,
-                        ),
-                      )
-                    : const Text("Calculate Cost"),
-                onPressed: () async {
-                  await viewModel.calculateRequest(
-                    dogNights: dogModel.nightsNumber,
-                    isDogGrooming: dogModel.isGrooming,
-                    catNights: catModel.nightsNumber,
-                    isCatGrooming: catModel.isGrooming,
                   );
-                },
-              )
-            ],
-          ),
+                }
+                return Container();
+              },
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            BlocBuilder<HomeBloc, HomeState>(
+              builder: (context, state) {
+                if (state is Loading) {
+                  return Container(
+                    margin: const EdgeInsets.all(5),
+                    child: const CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+                  );
+                  // } else if (state is Error) {
+                  //   return _showToast(context);
+                } else {
+                  return ElevatedButton(
+                    child: const Text("Calculated Cost"),
+                    onPressed: () {
+                      BlocProvider.of<HomeBloc>(context).add(
+                        CalculateEvent(
+                          dogNights: 0,
+                          isDogGrooming: false,
+                          catNights: 0,
+                          isCatGrooming: false,
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            )
+          ],
         ),
-      );
-    });
+      ),
+    );
+  }
+
+  void _showToast(BuildContext context) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: const Text('Added to favorite'),
+        action: SnackBarAction(
+            label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
+      ),
+    );
   }
 }
