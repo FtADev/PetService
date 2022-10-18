76a9705b55cb6a86e8cecaa4df4c7233e9aa83d6
Fatdev <akhlaghi.fatemeh@gmail.com>
2022-10-18 12:37:28 +0330

    Change response model

--- lib/service_cost/data/models/calculate_result_model.dart
+++ lib/service_cost/data/models/calculate_result_model.dart
@@ -4,13 +4,11 @@ class CostModel extends Cost{
 
   CostModel({totalPrice}) : super(totalPrice: totalPrice);
 
-  CostModel.fromJson(Map<String, dynamic> json) {
-    totalPrice = json['total_price'];
-  }
+  factory CostModel.fromJson(Map<String, dynamic> json) => CostModel(
+    totalPrice: json["total_price"],
+  );
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['total_price'] = totalPrice;
-    return data;
-  }
+  Map<String, dynamic> toJson() => {
+    "total_price": totalPrice,
+  };
 }
