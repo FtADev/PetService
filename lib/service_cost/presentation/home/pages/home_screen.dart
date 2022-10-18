7fc8be75e7e99f7c6e709a550766c0e4f28d0cf6
Fatdev <akhlaghi.fatemeh@gmail.com>
2022-10-18 20:08:56 +0330

    Don't show cost when loading

--- lib/service_cost/presentation/home/pages/home_screen.dart
+++ lib/service_cost/presentation/home/pages/home_screen.dart
@@ -26,6 +26,7 @@ class HomeScreen extends StatelessWidget {
               const SizedBox(
                 height: 50,
               ),
+              viewModel.isLoading ? Container() :
               viewModel.errorMessage == ""
                   ? Text(
                       "\$ ${viewModel.cost}",
